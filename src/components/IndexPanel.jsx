355f31f4af3e1ae63edbd53716cf7fe201bbc619
maustem <diego.maustem@gmail.com>
2024-12-13 17:09:18 -0300

    :sparkles: feat: Init implementing index page

---
+++ src/components/IndexPanel.jsx
@@ -0,0 +1,3 @@
+export default function IndexPanel() {
+  return <div>Painel Index</div>;
+}
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import "./App.css";
-import GoodForm from "./components/GoodForm.jsx";
+import GoodForm from "./components/GoodForm";
+import IndexPanel from "./components/IndexPanel";
 import { QueryClient, QueryClientProvider } from "react-query";
 
 const queryClient = new QueryClient();
@@ -11,6 +12,7 @@ createRoot(document.getElementById("root")).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <GoodForm />
+      <IndexPanel></IndexPanel>
     </QueryClientProvider>
   </StrictMode>
 );
